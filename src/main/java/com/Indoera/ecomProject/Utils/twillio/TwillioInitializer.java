efdd0e751be7916bdde02fa240b212819ff12e46
Shivam5197 <shivam511997@gmail.com>
2021-08-20 11:17:19 +0530

    Phone number issue solved

--- src/main/java/com/Indoera/ecomProject/Utils/twillio/TwillioInitializer.java
+++ src/main/java/com/Indoera/ecomProject/Utils/twillio/TwillioInitializer.java
@@ -1,43 +1,35 @@
 package com.Indoera.ecomProject.Utils.twillio;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import com.Indoera.ecomProject.Store.ServiceImpl.StoreServiceImpl;
 import com.twilio.Twilio;
 
 @Configuration
-public class TwillioInitializer /* implements CommandLineRunner */{
+public class TwillioInitializer{
 
-	private static final Logger logger = LoggerFactory.getLogger(TwillioInitializer.class);
+	private static final Logger logger = LogManager.getLogger(TwillioInitializer.class);
+	
 	private final TwillioProperties twillioProperties;
+
+	final String accountID = System.getenv("TWILIO_ACCOUNT_SID");
+	final String authId = System.getenv("TWILIO_AUTH_TOKEN");
 		
-	
-//	final String accountID = System.getenv("TWILIO_ACCOUNT_SID");
-//	final String authId = System.getenv("TWILIO_AUTH_TOKEN");
-//	logger.info("System Account ID------- "+ accountID);
-//	logger.info("System Auth ------- "+ authId);
-	
 //use these Variables	
 
 	
 	@Autowired
 	public TwillioInitializer(TwillioProperties twillioConfig) {
+//		logger.info("::::: Account ID :" +accountID);
+//		logger.info("::::: Account ID :" +authId);
+
 		this.twillioProperties = twillioConfig;
-		Twilio.init(twillioConfig.getAccountSid(), 
-						twillioConfig.getAuthToken());
+		Twilio.init(accountID, authId);
 		
-
-//	logger.info("Account S ID env ============== "+ env.getProperty("TWILIO_ACCOUNT_SID"));	
-//	logger.info("Account auth token env ============== "+ env.getProperty("TWILIO_AUTH_TOKEN"));	
-
-	
-//	logger.info("Account Sid: :::::::::::::: " + twillioConfig.getAccountSid() );
-//		logger.info("Account Sid: :::::::::::::::::: " + twillioConfig.getAuthToken() );
 	}
 	
 }
